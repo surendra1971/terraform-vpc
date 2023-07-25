838ef2d055540c2c09d777e1ed59ec88a0bc6706
Your Name <you@example.com>
2023-07-25 23:18:19 +0530

    updating the Auto Push

--- main.tf
+++ main.tf
@@ -2,5 +2,8 @@ module "vpc" {
   source              = "./vendor/modules/vpc"
   VPC_CIDR            = var.VPC_CIDR
   ENV                 = var.ENV
+  AZ                  = var.AZ 
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR 
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
 # You cannot variablize this piece of code in the source parameter
